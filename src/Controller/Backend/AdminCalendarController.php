19a28d20f0bf17396018163de5655d58f0d29f62
SEB <sebastien@linaseb.fr>
2019-09-13 15:13:50 +0200

    Vue Hebdo - changement semaine +/- = OK

--- src/Controller/Backend/AdminCalendarController.php
+++ src/Controller/Backend/AdminCalendarController.php
@@ -3,7 +3,7 @@
 namespace App\Controller\Backend;
 
 use App\Utils\Calendar\Month;
-use App\Utils\Calendar\Week2;
+use App\Utils\Calendar\Week;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -75,153 +75,31 @@ class AdminCalendarController extends AbstractController
         ]);
     }
 
+
     /**
      * @Route("/admin/calendar/week", name="admin_calendar_week")
      */
     public function week()
     {
 
-          $month = new Month($_GET['month'] ?? null, $_GET['year'] ?? null);
-
-          $week = new Week($_GET['week'] ?? null, $_GET['day'] ?? null, $_GET['month'] ?? null, $_GET['year'] ?? null);
-
-          $start = $month->getStartingDay();
-          
-          $launch_day = $start->format('N');
-          // $start = $start->format('N') === '1' ? $start_monday = $start : $start_other = $month->getStartingDay()->modify('last monday');
-
-          $start_other = $month->getStartingDay()->modify('last monday');
-          // var_dump($start_other);
-
-          
-          
-
-          // $end = $month->getEndingDay();
-          // $end = $end->format('d');
-
-          $month_name = $month->toString();
-
-          // $date = $month->getDate();
-
-
-          $previous_month_year = $month->previousMonth()->year;
-
-          $next_month_month = $month->nextMonth()->month;
-          $next_month_year = $month->nextMonth()->year;
-
-
-          $month_days = $month->days;
-          
-          
-
-
-
-          $days = $month->days;
-
-
-          
-          
-          $start_monday = $start->format('d');
-          $start_other = $start_other->format('d');
-    
-          $week_starting_day = $week->getWeekStartingDay()->format('d');
-          $week_ending_day = $week->getWeekEndingDay()->format('d');
-
-          $week_number = $week->getWeekStartingDay()->format('W');
-          // $year_number = $week->getWeekStartingDay()->format('Y');
-
-          if($week_ending_day > $week_starting_day) {
-            $month_number = $week->getWeekStartingDay()->format('m');
-            // $week_number = $week->getWeekStartingDay()->format('W');
-            $year_number = $week->getWeekStartingDay()->format('Y');
-          } else {
-            $month_number = $week->getWeekEndingDay()->format('m');
-            // $week_number = ((clone $week->getWeekEndingDay())->modify("-" . 1 . "week"))->format('W');
-            $year_number = $week->getWeekEndingDay()->format('Y');
-          }
-
-          if($week_ending_day > $week_starting_day) {
-            $nb_days_month = $week->getWeekStartingDay()->format('t');
-          } else {
-            $nb_days_month = $week->getWeekEndingDay()->format('t');
-          }
-
-var_dump($week->getWeekStartingDay());
-var_dump($week->getWeekStartingDay()->format('W'));
-var_dump($week->getWeekStartingDay()->format('Y'));
-var_dump($week->getWeekEndingDay());
-var_dump($week->getWeekEndingDay()->format('W'));
-var_dump($week->getWeekEndingDay()->format('Y'));
-
-          // ((clone $week->getWeekEndingDay())->modify("-" . 1 . "month"))->format('t');
-          // $week->getWeekStartingDay()->format('t');
-
-          if($week_ending_day > $week_starting_day) {
-            $previous_month_month = ((clone $week->getWeekStartingDay())->modify("-" . 1 . "month"))->format('m');  
-          } else {
-            $previous_month_month = $week->getWeekStartingDay()->format('m');
-          }
-          
-          if($week_ending_day > $week_starting_day) {
-            $nb_days_previous_month = ((clone $week->getWeekStartingDay())->modify("-" . 1 . "month"))->format('t');  
-          } else {
-            $nb_days_previous_month = $week->getWeekStartingDay()->format('t');
-          }
-
-          $previous_week_number = $week->getPreviousWeek()->week;
-          $next_week_number = $week->getNextWeek()->week;
-          
-
-
-
-        return $this->render('calendar/week.html.twig', [
-            'month_name' => $month_name,
-            'previous_month_month' => $previous_month_month,
-            'previous_month_year' => $previous_month_year,
-            'next_month_month' => $next_month_month,
-            'next_month_year' => $next_month_year,
-            'month_days' => $month_days,
-            'days' => $days,
-            'start_monday' => $start_monday,
-            'launch_day' => $launch_day,
-            'start_other' => $start_other,
-            'initial_start' => $start,
-            'nb_days_month' => $nb_days_month,
-            'nb_days_previous_month' => $nb_days_previous_month,
-            'month_number' => $month_number,
-            'year_number' => $year_number,
-            'week_number' => $week_number,
-            'previous_week_number' => $previous_week_number,
-            'next_week_number' => $next_week_number,
-            'week_starting_day' => $week_starting_day,
-            'week_ending_day' => $week_ending_day,
-        ]);
-    }
-
-    /**
-     * @Route("/admin/calendar/week2", name="admin_calendar_week2")
-     */
-    public function week2()
-    {
-
       // Si la semaine n'est pas fournie, on utilise la semaine correspondant à la date du jour système
       if(!isset($_GET['week'])) {
         // $week = new \DateTime();
-        $week = new Week2();
+        $week = new Week();
         $week = $week->today;
         $actual_week = $week->format('W');
       } else {
-        $week = new Week2();
+        $week = new Week();
         $actual_week = $week->week;
         $actual_week = $_GET['week'];
       };
 
       if(!isset($_GET['year'])) {
-        $year = new Week2();
+        $year = new Week();
         $year = $year->today;
         $actual_year = $year->format('Y');
       } else {
-        $year = new Week2();
+        $year = new Week();
         $actual_year = $year->year;
         $actual_year = $_GET['year'];
       };
@@ -244,75 +122,55 @@ var_dump($week->getWeekEndingDay()->format('Y'));
         $next_week_year = $actual_year;  
       }
 
-      // var_dump($previous_week_week);
-      // var_dump($previous_week_year);
+      // ***** 1er jour à afficher dans le calendrier hebdomadaire *****
 
-      // var_dump($actual_week);
-      // var_dump($actual_year);
+        $base_day = new Week();
+        $base_day = new \DateTime("{$actual_year}-01-01");  // Le 1er Janvier de l'année en cours
+        $base_number = $base_day->format('N');        // Le numéro du jour du 1er Janvier de l'année en cours
+        
+        // Calcul du décalage de semaine
+        if($actual_week == 1) {
+          $shift_number = $actual_week - 1;
+        } else {
+          $shift_number = $actual_week - 1;
+        }
 
-      // var_dump($next_week_week);
-      // var_dump($next_week_year);die();
+        // 1er jour de la semaine recherchée
+        if($base_number == 1) {
+          $week_starting_day = $base_day;
+        } else {
+          $week_starting_day = ((clone $base_day)->modify("+" . $shift_number . "weeks"))->modify('last monday');
+        }
+
+
+      // Récupère les jours à afficher dans le tableau hebdomadaire
+
       
-          
-      // Date du lendemain
-      // $date_of_tomorrow = (clone $date_of_day)->modify("+" . 1 . "day")->format('Y-m-d');
-
-      // Date du jour (au bon format pour renvoi à la vue)
-      // Ne pas remonter cet élément au dessus des calculs de dates de la veille et du lendemain
-      // $date_of_day = $date_of_day->format('d-m-Y');
-
-      // DEFINITION DES VALEURS A ZERO POUR RENVOI A LA VUE
-      $month_name = 0;
-      $previous_month_month = 0;
-      $previous_month_year = 0;
-      $next_month_month = 0;
-      $next_month_year = 0;
-      $month_days = 0;
-      $days = 0;
-      $start_monday = 0;
-      $launch_day = 0;
-      $start_other = 0;
-      $start = 0;
-      $nb_days_month = 0;
-      $nb_days_previous_month = 0;
-      $month_number = 0;
-      $year_number = 0;
-      $week_number = 0;
-      $previous_week_number = 0;
-      $next_week_number = 0;
-      $week_starting_day = 0;
-      $week_ending_day = 0;
+      $week_day_1 = (clone $week_starting_day)->format('d/m/Y');
+      $week_day_2 = (clone $week_starting_day)->modify("+" . 1 . "day")->format('d/m/Y');
+      $week_day_3 = (clone $week_starting_day)->modify("+" . 2 . "day")->format('d/m/Y');
+      $week_day_4 = (clone $week_starting_day)->modify("+" . 3 . "day")->format('d/m/Y');
+      $week_day_5 = (clone $week_starting_day)->modify("+" . 4 . "day")->format('d/m/Y');
+      $week_day_6 = (clone $week_starting_day)->modify("+" . 5 . "day")->format('d/m/Y');
+      $week_day_7 = (clone $week_starting_day)->modify("+" . 6 . "day")->format('d/m/Y');
+      
 
 
-
-        return $this->render('calendar/week2.html.twig', [
+        return $this->render('calendar/week.html.twig', [
             'actual_week' => $actual_week,
             'actual_year' => $actual_year,
             'previous_week_week' => $previous_week_week,
             'previous_week_year' => $previous_week_year,
             'next_week_week' => $next_week_week,
             'next_week_year' => $next_week_year,
+            'week_day_1' => $week_day_1,
+            'week_day_2' => $week_day_2,
+            'week_day_3' => $week_day_3,
+            'week_day_4' => $week_day_4,
+            'week_day_5' => $week_day_5,
+            'week_day_6' => $week_day_6,
+            'week_day_7' => $week_day_7,
 
-            'month_name' => $month_name,
-            'previous_month_month' => $previous_month_month,
-            'previous_month_year' => $previous_month_year,
-            'next_month_month' => $next_month_month,
-            'next_month_year' => $next_month_year,
-            'month_days' => $month_days,
-            'days' => $days,
-            'start_monday' => $start_monday,
-            'launch_day' => $launch_day,
-            'start_other' => $start_other,
-            'initial_start' => $start,
-            'nb_days_month' => $nb_days_month,
-            'nb_days_previous_month' => $nb_days_previous_month,
-            'month_number' => $month_number,
-            'year_number' => $year_number,
-            'week_number' => $week_number,
-            'previous_week_number' => $previous_week_number,
-            'next_week_number' => $next_week_number,
-            'week_starting_day' => $week_starting_day,
-            'week_ending_day' => $week_ending_day,
         ]);
     }
 
