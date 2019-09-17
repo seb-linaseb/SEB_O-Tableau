7d5612aba1c91bed7138cdb756960d8509082179
SEB <sebastien@linaseb.fr>
2019-09-17 18:02:56 +0200

    update cantine

--- src/Controller/Backend/AdminCalendarController.php
+++ src/Controller/Backend/AdminCalendarController.php
@@ -3,9 +3,11 @@
 namespace App\Controller\Backend;
 
 use App\Entity\Student;
+use App\Entity\Calendar;
 use App\Entity\Classroom;
 use App\Utils\Calendar\Week;
 use App\Utils\Calendar\Month;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\ClassroomRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,9 +15,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminCalendarController extends AbstractController
 {
-
+public $daynumber;
     /**
-     * @Route("canteen/month", name="canteen_month")
+     * @Route("admin/calendar/month", name="admin_calendar_month")
      */
     public function month()
     {
@@ -80,12 +82,9 @@ class AdminCalendarController extends AbstractController
            }
 
 
-          foreach ($days_of_month as $i => $value) {
-            // dump($value);
-          }
-          // die();
+          
 
-        return $this->render('calendar/month.html.twig', [
+        return $this->render('backend/calendar/month.html.twig', [
             'month_name' => $month_name,
             'previous_month_month' => $previous_month_month,
             'previous_month_year' => $previous_month_year,
@@ -107,73 +106,41 @@ class AdminCalendarController extends AbstractController
             'month_number' => $month_number,
             'year_number' => $year_number,
         ]);
+
+        
+        
     }
 
-
     /**
-     * @Route("canteen/month_old", name="canteen_month_old")
+     * @Route("admin/calendar/new", name="admin_calendar_new", methods={"GET","POST"} )
      */
-    public function month_old()
+    public function new()
     {
-        // try {
-          $month = new Month($_GET['month'] ?? null, $_GET['year'] ?? null);
-        // } catch (\Exception $e) {
-          // $month = new App\Date\Month();
-        // }
-        
-          $start = $month->getStartingDay();
-          $launch_day = $start->format('N');
-          // $start = $start->format('N') === '1' ? $start_monday = $start : $start_other = $month->getStartingDay()->modify('last monday');
+      
+      $dates = $_POST;
+      // dump($dates);
+      // die();
 
-          $start_other = $month->getStartingDay()->modify('last monday');
+      foreach ($dates as $key => $value) {
+        if($value == 'on'){
+          //dump($key);
+          $calendar = new Calendar();
+          $calendar->setDate(\DateTime::createFromFormat('d/m/Y', $key));
+          $calendar->setIsWorked(true);
+          $entityManager = $this->getDoctrine()->getManager();
+          $entityManager->persist($calendar);
+          $entityManager->flush();
+          //dump($calendar);
+        }
+      
+      }
+      $this->addFlash(
+        'success',
+        'Enregistrement effectué'
+    );
 
-          $end = $month->getEndingDay();
-          $end = $end->format('d');
 
-          $month_name = $month->toString();
-
-          // $date = $month->getDate();
-
-          $previous_month_month = $month->previousMonth()->month;
-          $previous_month_year = $month->previousMonth()->year;
-
-          $next_month_month = $month->nextMonth()->month;
-          $next_month_year = $month->nextMonth()->year;
-
-          $nb_weeks = $month->getWeeks();
-
-          $month_days = $month->days;
-          $month_number = $month->getEndingDay()->format('m');
-
-          $year_number = $start->format('Y');
-
-          $days = $month->days;
-          $nb_days_month = $month->getStartingDay()->format('t');
-
-          $nb_days_previous_month = (clone $start)->modify("-" . 1 . "day")->format('t');
-          
-          $start_monday = $start->format('d');
-          $start_other = $start_other->format('d');
-    
-
-        return $this->render('calendar/month_old.html.twig', [
-            'month_name' => $month_name,
-            'previous_month_month' => $previous_month_month,
-            'previous_month_year' => $previous_month_year,
-            'next_month_month' => $next_month_month,
-            'next_month_year' => $next_month_year,
-            'nb_weeks' => $nb_weeks,
-            'month_days' => $month_days,
-            'days' => $days,
-            'start_monday' => $start_monday,
-            'launch_day' => $launch_day,
-            'start_other' => $start_other,
-            'initial_start' => $start,
-            'nb_days_month' => $nb_days_month,
-            'nb_days_previous_month' => $nb_days_previous_month,
-            'month_number' => $month_number,
-            'year_number' => $year_number,
-        ]);
+      return $this->redirectToRoute('admin_calendar_month');
     }
 
 
